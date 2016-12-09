520ea75bf68d46529fa3cafdc14c1b745370cfd5
kjablonska <kjablonska@ownedoutcomes.com>
2016-12-09 15:41:37 +0100

    styles added

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    photo = models.ImageField(upload_to='blog/%Y/%m/%d/', null=True, blank=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
